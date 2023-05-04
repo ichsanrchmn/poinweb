01af20d7b07df990ecb3c09404dd8583e99dd867
ichsanrchmn <ichsanugraha@gmail.com>
2023-05-04 16:14:52 +0700

    mengubah slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,29 +1,38 @@
-currentSlideID = 1;
+// currentSlideID = 1;
 
-sliderElement = document.getElementById('slider');
-totalSlides = sliderElement.childElementCount;
+// sliderElement = document.getElementById('slider');
+// totalSlides = sliderElement.childElementCount;
 
+// function next() {
+// 	if (currentSlideID < totalSlides)
+// 	currentSlideID++;
+// 	showSlide()
+// }
 
-function next() {
-	if (currentSlideID < totalSlides)
-	currentSlideID++;
-	showSlide()
-}
+// function prev() {
+// 	if (currentSlideID > 1)
+// 	currentSlideID--;
+// 	showSlide()
+// }
 
-function prev() {
-	if (currentSlideID > 1)
-	currentSlideID--;
-	showSlide()
-}
+// function showSlide() {
+//   slides = document.getElementById('slider').getElementsByTagName('li')
+//   for (let i = 0; i < totalSlides; i++) {
+//     const element = slides[i];
+//     if (currentSlideID===i+1) {
+//       element.classList.remove('hidden');
+// 			element.classList.add('ease-in-out');
+//     } else {
+//       element.classList.add('hidden');
+//     }
+//   }
+// }
 
-function showSlide() {
-	slides = document.getElementById('slider').getElementsByTagName('li')
-	for (let i = 0; i < totalSlides; i++) {
-		const element = slides[i];
-		if (currentSlideID===i+1) {
-			element.classList.remove('hidden');
-		} else {
-			element.classList.add('hidden');
-		}
-	}
-}
+let counter = 1;
+setInterval(function () {
+  document.getElementById("nav" + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 4000);
